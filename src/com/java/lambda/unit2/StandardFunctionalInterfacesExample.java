0a62ac14a4a8f6a340f095c481790d9c597be7d4
Ramya PC <ramya.pc@accoliteindia.com>
2020-09-08 15:21:56 +0530

    more functional interface

--- src/com/java/lambda/unit2/StandardFunctionalInterfacesExample.java
+++ src/com/java/lambda/unit2/StandardFunctionalInterfacesExample.java
@@ -5,6 +5,7 @@ import com.java.lambda.unit1.Person;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class StandardFunctionalInterfacesExample {
@@ -22,23 +23,23 @@ public class StandardFunctionalInterfacesExample {
 
         //Step 2: Create method that prints all the element in list
         // printAllPeople(people);
-        printConditionally(people, p ->true);
+        printConditionally(people, p ->true, p -> System.out.println(p));
 
         //Step 3: Create a method that prints all people that have last name beginning with C
 
         System.out.println("Last Name Starts with C from Condition check");
         System.out.println("---------------------------------------------");
-        printConditionally(people, p -> p.getLastName().startsWith("C"));
+        printConditionally(people, p -> p.getLastName().startsWith("C"), p -> System.out.println(p));
         System.out.println("First Name Starts with C from Condition check");
         System.out.println("---------------------------------------------");
-        printConditionally(people, p -> p.getFirstName().startsWith("R"));
+        printConditionally(people, p -> p.getFirstName().startsWith("R"), p -> System.out.println(p));
 
     }
 
-    private static void printConditionally(List<Person> people, Predicate<Person> personPredicate) {
+    private static void printConditionally(List<Person> people, Predicate<Person> personPredicate, Consumer<Person> consumer) {
         for(Person p:people){
             if(personPredicate.test(p)){
-                System.out.println(p);
+                consumer.accept(p);
             }
         }
         System.out.println();
