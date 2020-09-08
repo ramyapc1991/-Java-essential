c6e8f93ebc967e61524b2b8d7699c5465e195b6a
Ramya PC <ramya.pc@accoliteindia.com>
2020-09-08 17:30:43 +0530

    Exception handling approach

--- src/com/java/lambda/unit2/ExceptionHandlingExample.java
+++ src/com/java/lambda/unit2/ExceptionHandlingExample.java
@@ -7,13 +7,7 @@ public class ExceptionHandlingExample {
         int[] someNumbers = {1,2,3,4,5};
         int key =0;
 
-        process(someNumbers, key, (v,k) -> {
-            try{
-                System.out.println(v / k);
-            }catch (ArithmeticException e){
-                System.out.println("An Arithmetic Exception happened");
-            }
-        });
+        process(someNumbers, key, wrapperLambda((v,k) -> System.out.println(v/k)));
     }
 
     private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer) {
@@ -21,4 +15,15 @@ public class ExceptionHandlingExample {
             consumer.accept(i,key);
         }
     }
+
+    public static BiConsumer<Integer, Integer> wrapperLambda (BiConsumer<Integer, Integer> consumer){
+        return (v,k) -> {
+                try{
+                    consumer.accept(v, k);
+                }catch (ArithmeticException e){
+                    System.out.println("Exception catch block in wrapper lambda method");
+                }
+
+            };
+        }
 }
