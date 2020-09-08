d5ffde485c7819b97c45a6e1dfaad564f61a12a1
Ramya PC <ramya.pc@accoliteindia.com>
2020-09-08 17:11:50 +0530

    Exception handling in lambda

---
+++ src/com/java/lambda/unit2/ExceptionHandlingExample.java
@@ -0,0 +1,24 @@
+package com.java.lambda.unit2;
+
+import java.util.function.BiConsumer;
+
+public class ExceptionHandlingExample {
+    public static void main(String[] args){
+        int[] someNumbers = {1,2,3,4,5};
+        int key =0;
+
+        process(someNumbers, key, (v,k) -> {
+            try{
+                System.out.println(v / k);
+            }catch (ArithmeticException e){
+                System.out.println("An Arithmetic Exception happened");
+            }
+        });
+    }
+
+    private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer) {
+        for(Integer i :someNumbers){
+            consumer.accept(i,key);
+        }
+    }
+}
